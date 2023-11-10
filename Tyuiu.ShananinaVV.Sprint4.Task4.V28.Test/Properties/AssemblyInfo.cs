03a3df2ef10e671e9195557e9a76c169330d228e
Tyuiu.ShananinaVV@outlook.com <valeriya.v.2508@gmail.com>
2023-11-10 23:18:44 +0500

    Task4

---
+++ Tyuiu.ShananinaVV.Sprint4.Task4.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShananinaVV.Sprint4.Task4.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShananinaVV.Sprint4.Task4.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("693cff68-c0e9-462c-a42d-5de1ef359144")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShananinaVV.Sprint4.Task4.V28/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShananinaVV.Sprint4.Task4.V28.Lib;
+
+namespace Tyuiu.ShananinaVV.Sprint4.Task4.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Шананина В. В. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы (ввод с клавиатуры)                             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнила: Шананина Валерия Викторовна | ИСТНб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 1 до 5.                          *");
+            Console.WriteLine("* Заменить четные элементы массива на 1.                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите количество столбцов в массиве: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mtrx = new int[rows, columns];
+
+            Console.WriteLine("***************************************************************************");
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"Введите {i},{j} элемент массива: ");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Итоговый массив: ");
+            int[,] res = ds.Calculate(mtrx);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{res[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+           
+    }
+}
